83850400c5d1386020dbdc64395d8c8968faf954
Austin Vickers <adv0003@uah.edu>
2017-01-24 13:54:41 -0600

    Oculus Touch Implemented
    Stuuf goes here
    4

--- SMAP Center 01/Assets/Scripts/SetPosToPlayer.cs
+++ SMAP Center 01/Assets/Scripts/SetPosToPlayer.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OculusSelect : MonoBehaviour {
+public class SetPosToPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    public Transform player;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.position = player.position;
 	}
 }
---
+++ SMAP Center 01/Assets/Scripts/TouchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VR = UnityEngine.VR;
+
+public class TouchController : MonoBehaviour
+{
+
+    public OVRInput.Controller controller;
+    // public OVRPlayerController player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(controller) + new Vector3(0f, 0.5f, 0f);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+    }
+}
